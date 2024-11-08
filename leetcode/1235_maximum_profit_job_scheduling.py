81fccbfa98295e6ec898807c390ae486c27a892c
mgzik <mgzik@eversis.com>
2024-11-08 11:19:03 +0100

    Add unit tests for job scheduling solution and refactor code

--- leetcode/1235_maximum_profit_job_scheduling.py
+++ leetcode/1235_maximum_profit_job_scheduling.py
@@ -1,31 +1,23 @@
 from typing import List
-
+import unittest
 
 class Solution:
     def jobScheduling(self, start_times: List[int], end_times: List[int], profits: List[int]) -> int:
         # Combine the job information into a single list and sort by end time.
         jobs = sorted(zip(end_times, start_times, profits))
-      
-        # Get the total number of jobs.
         number_of_jobs = len(profits)
-      
-        # Initialize dynamic programming table with 0 profits.
         dp = [0] * (number_of_jobs + 1)
       
-        # Iterate over the jobs.
         for i, (current_end_time, current_start_time, current_profit) in enumerate(jobs):
             # Find the rightmost job that doesn't conflict with the current job's start time.
-            # Use binary search for efficient querying. 'hi' is set to the current index 'i' for optimization.
             index = self.bisect_right(jobs, current_start_time, hi=i, key=lambda x: x[0])
-          
             # Update the DP table by choosing the maximum of either taking the current job or not.
-            # If taking the current job, add its profit to the total profit of non-conflicting jobs.
             dp[i + 1] = max(dp[i], dp[index] + current_profit)
       
-        # Return the maximum profit which is the last element in the DP table.
         return dp[number_of_jobs]
     
     def bisect_right(self, arr, target, lo=0, hi=None, key=lambda x: x):
+        """Helper function to perform binary search."""
         if hi is None:
             hi = len(arr)
         while lo < hi:
@@ -35,11 +27,22 @@ class Solution:
             else:
                 hi = mid
         return lo
-    
 
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_case_1(self):
+        self.assertEqual(self.solution.jobScheduling([1, 2, 3, 4, 6], [3, 5, 10, 6, 9], [20, 20, 100, 70, 60]), 150)
+
+    def test_case_2(self):
+        self.assertEqual(self.solution.jobScheduling([1, 2, 3, 3], [3, 4, 5, 6], [50, 10, 40, 70]), 120)
+
+    def test_case_3(self):
+        self.assertEqual(self.solution.jobScheduling([1, 1, 1], [2, 3, 4], [5, 6, 4]), 6)
+
+    def test_case_4(self):
+        self.assertEqual(self.solution.jobScheduling([4, 2, 4, 8, 2], [5, 5, 5, 10, 8], [1, 2, 8, 10, 4]), 18)
 
 if __name__ == "__main__":
-    solution = Solution()
-    print(
-        solution.jobScheduling([1, 2, 3, 4, 6], [3, 5, 10, 6, 9], [20, 20, 100, 70, 60])
-    )
+    unittest.main()
